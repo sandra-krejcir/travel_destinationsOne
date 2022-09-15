f5e83e6705499df384d4d0da3311e7f8d242c1b8
Sandra Krejcir <krejcir.sandra@gmail.com>
2022-09-15 11:44:46 +0200

    made the mongodb connection work

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb").MongoClient;
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-fs.readFile("./index.html", function (err, html) {
-  if (err) {
-    throw err;
+let db;
+let connectionString =
+  "mongodb+srv://krejcir_sandra:54Ndr42810@cph-webdev.6nfcpoh.mongodb.net/travel_project";
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  function (err, client) {
+    db = client.db();
+    db.collection("destinations").findOne({}, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        res.write("Hello");
+        res.end();
+      });
+
+      server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+      });
+    });
   }
-
-  const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write(html);
-    res.end();
-  });
-
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
-});
+);
