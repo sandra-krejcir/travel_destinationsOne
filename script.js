ca10ee59b1db15dfe999ace40a3e6e6ef6e50eb2
Sandra Krejcir <krejcir.sandra@gmail.com>
2022-09-15 09:54:11 +0200

    connected the nodeJS with html

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-});
+fs.readFile("./index.html", function (err, html) {
+  if (err) {
+    throw err;
+  }
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.write(html);
+    res.end();
+  });
+
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
 });
