bf19ab7a5fc41c9b194a70873885f5a323b8c282
Sandra Krejcir <krejcir.sandra@gmail.com>
2022-09-22 14:44:20 +0200

    made the destinations list

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
 const url = "http://127.0.0.1:3000/";
 
-const destination = {
-  title: "New code, how bout now 2",
-  dateFrom: 2022 - 10 - 12,
-  dateTo: 2022 - 10 - 15,
-  description: "blabla",
-  location: "here",
-  country: "neverland",
-  picture: "url",
-};
-
 window.addEventListener("load", async () => {
-  console.log("hello");
-  /* await getData(); */
-  await putData();
+  const data = await getData();
+  console.log(data);
+  /*  await putData(); */
   /*  await postData(); */
-});
 
-/* async function getData() {
-  const response = await fetch(url);
-  const body = await response.json();
-  console.log(body);
-  return body;
-} */
+  data.forEach((contact) => showDestinations(contact));
 
-async function putData() {
+  function showDestinations(contact) {
+    const template = document.querySelector("#theList").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector("div").setAttribute("id", contact._id);
+    copy.querySelector("h2").textContent = contact.title;
+    copy.querySelector("p").textContent = contact.description;
+    copy.querySelector("#edit-button").addEventListener("click", () => {
+      window.location.replace(
+        "http://127.0.0.1:3000/update.html?id=" + contact._id
+      );
+    });
+    const parent = document.querySelector("#destinationList");
+    parent.appendChild(copy);
+  }
+
+  async function getData() {
+    const response = await fetch(url);
+    const body = await response.json();
+    console.log(body);
+    return body;
+  }
+
+  /* async function putData() {
   const response = await fetch(url + "632c3f1be961e63b4bd0e5a4", {
     method: "PUT",
     body: JSON.stringify(destination),
     headers: { "Content-Type": "application/json" },
   });
 }
-
-/* async function postData() {
+ */
+  /* async function postData() {
   const response = await fetch(url, {
     method: "POST",
     body: JSON.stringify(destination),
@@ -40,3 +46,4 @@ async function putData() {
   });
   console.log(response);
 } */
+});
